e5418928eeea69fe874ab47bdd12cfece34153ea
Suyash Ranjan <137883487+suyash3110@users.noreply.github.com>
2026-07-03 17:52:31 +0530

    shots.py

--- few_shots.py
+++ few_shots.py
@@ -22,7 +22,7 @@ group by t_shirt_id) a left join discounts on a.t_shirt_id = discounts.t_shirt_i
      'SQLQuery' : "SELECT sum(stock_quantity) FROM t_shirts WHERE brand = 'Levi' AND color = 'White'",
      'SQLResult': "Result of the SQL query",
      'Answer' : "290"
-     },
+     } ,
     {'Question': "how much sales amount will be generated if we sell all large size t shirts today in nike brand after discounts?",
      'SQLQuery' : """SELECT sum(a.total_amount * ((100-COALESCE(discounts.pct_discount,0))/100)) as total_revenue from
 (select sum(price*stock_quantity) as total_amount, t_shirt_id from t_shirts where brand = 'Nike' and size="L"
